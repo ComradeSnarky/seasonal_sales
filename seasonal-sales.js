271f841a302d45d1047b2f2cb8255b1b599e64ea
Lauren Kight <kight6@gmail.com>
2016-07-27 11:38:32 -0500

    added select element to html and pull season sales from json to select

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+var categories = (function() {
+  return {
+    getSeasons: function(callBack) {
+      var xhr = new XMLHttpRequest()
+        xhr.open('GET', 'categories.json')
+        xhr.addEventListener('load', function(evt){
+          var seasonal = JSON.parse(evt.target.responseText)
+          callBack(seasonal)
+        })
+        xhr.send()
+    }
+  }
+})();
+
+function showCategories (categories) {
+  var list = document.querySelector('.seasonalDiscounts')
+  console.log(categories)
+  categories.categories.forEach(function (category) {
+    list.innerHTML += `<option>${category.season_discount}</option>`
+  })
+}
+console.log(categories)
+categories.getSeasons(showCategories);
